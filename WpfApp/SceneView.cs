b3ef263bc76ecfae9bb990cb267244029c28110f
a359807859 <359807859@qq.com>
2019-03-11 14:21:24 +0800

    base file

---
+++ WpfApp/SceneView.cs
@@ -0,0 +1,38 @@
+﻿// // yhh - 
+// // WpfApp - WpfApp - SceneView.cs
+// // 2019 - 03 - 11 - 14:14
+// // 2019 - 03 - 11 - 14:14
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Framework.WpfInterop;
+
+namespace WpfApp
+{
+	public class SceneView : WpfGame
+	{
+		private IGraphicsDeviceService _graphicsDeviceService;
+		private SpriteBatch spriteBatch;
+		private Texture2D texture;
+
+		protected override void Initialize()
+		{
+			_graphicsDeviceService = new WpfGraphicsDeviceService(this);
+
+			base.Initialize();
+
+			Content.RootDirectory = "Content";
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+			texture = Content.Load<Texture2D>("10000");
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			base.Draw(gameTime);
+
+			spriteBatch.Begin();
+			spriteBatch.Draw(texture, Vector2.Zero, Color.White);
+			spriteBatch.End();
+		}
+	}
+}
